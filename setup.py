1c2ffc977ac7cc7474aa9f016efa3cabf91c7554
Andrey Gubarev <mylokin@me.com>
2015-02-16 01:41:26 +0300

    make flake8 happy

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name = 'redisext',
-    packages = ['redisext', 'redisext.backend'],
-    version = '1.0.1',
-    description = 'Data models for Redis',
-    author = 'Andrey Gubarev',
-    author_email = 'mylokin@me.com',
-    url = 'https://github.com/mylokin/redisext',
-    keywords = ['redis', 'orm', 'models'],
-    classifiers = [],
+    name='redisext',
+    packages=['redisext', 'redisext.backend'],
+    version='1.0.1',
+    description='Data models for Redis',
+    author='Andrey Gubarev',
+    author_email='mylokin@me.com',
+    url='https://github.com/mylokin/redisext',
+    keywords=['redis', 'orm', 'models'],
+    classifiers=[],
 )
