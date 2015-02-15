08ca508f3b56b7aa029ba487e1446900015b4d23
Andrey Gubarev <mylokin@me.com>
2015-02-16 00:41:09 +0300

    add m/s to pool #3

--- redisext/pool.py
+++ redisext/pool.py
@@ -11,13 +11,13 @@ class Pool(object):
 
     @classmethod
     def pop(cls, key):
-        item = cls.connect().spop(key)
+        item = cls.connect_to_master().spop(key)
         return redisext.utils.decode(cls, item)
 
     @classmethod
     def push(cls, key, item):
         item = redisext.utils.encode(cls, item)
-        return cls.connect().sadd(key, item)
+        return cls.connect_to_master().sadd(key, item)
 
 
 class SortedSet(object):
@@ -27,15 +27,15 @@ class SortedSet(object):
     @classmethod
     def add(cls, key, element, score):
         element = redisext.utils.encode(cls, element)
-        cls.connect().zadd(key, score, element)
+        cls.connect_to_master().zadd(key, score, element)
 
     @classmethod
     def length(cls, key, start_score, end_score):
-        return cls.connect().zcount(key, start_score, end_score)
+        return cls.connect_to_slave().zcount(key, start_score, end_score)
 
     @classmethod
     def members(cls, key):
-        elements = cls.connect().zrevrange(key, 0, -1)
+        elements = cls.connect_to_slave().zrevrange(key, 0, -1)
         if not elements:
             return elements
 
@@ -45,12 +45,12 @@ class SortedSet(object):
     @classmethod
     def contains(cls, key, element):
         element = redisext.utils.encode(cls, element)
-        return cls.connect().zscore(key, element) is not None
+        return cls.connect_to_slave().zscore(key, element) is not None
 
     @classmethod
     def truncate(cls, key, size):
-        return cls.connect().zremrangebyrank(key, 0, -1 * size - 1)
+        return cls.connect_to_master().zremrangebyrank(key, 0, -1 * size - 1)
 
     @classmethod
     def clean(cls, key):
-        return cls.connect().delete(key)
+        return cls.connect_to_master().delete(key)
