9ebe59326a103f609621b167f8d171997bde3cbb
Andrey Gubarev <mylokin@me.com>
2015-02-16 00:57:29 +0300

    add dummy connection master/slave test #3

--- redisext/tests/test_client.py
+++ redisext/tests/test_client.py
@@ -3,13 +3,29 @@ from __future__ import absolute_import
 import unittest
 
 import redisext.backend.abc
+import redisext.backend.redis
 
 
 class Connection(redisext.backend.abc.IConnection):
     MASTER = {'host': 'localhost', 'port': 6379, 'db': 0}
 
 
-class TestCase(unittest.TestCase):
+class IConnectionTestCase(unittest.TestCase):
     def test_abstract_connection(self):
         with self.assertRaises(NotImplementedError):
             Connection.connect_to_master()
+
+
+class ReplicatedConnection(redisext.backend.redis.Connection):
+    MASTER = {'host': 'localhost', 'port': 6379, 'db': 0}
+    SLAVE = {'host': 'localhost', 'port': 6379, 'db': 1}
+
+
+class ReplicatedConnectionTestCase(unittest.TestCase):
+    def tearDown(self):
+        ReplicatedConnection.connect_to_master().flushdb()
+        ReplicatedConnection.connect_to_slave().flushdb()
+
+    def test_slave(self):
+        ReplicatedConnection.connect_to_master().set('key', 'value')
+        self.assertIsNone(ReplicatedConnection.connect_to_slave().get('key'))
