15788064fcedc334e31ecd218f8e9d95283049c2
Andrey Gubarev <mylokin@me.com>
2015-02-15 04:01:39 +0300

    extend fixtures

--- redisext/tests/fixture.py
+++ redisext/tests/fixture.py
@@ -1,7 +1,14 @@
 from __future__ import absolute_import
 
+import unittest
+
 import redisext.backend.redis
 
 
 class Redis(redisext.backend.redis.Redis):
     SETTINGS = {'host': 'localhost', 'port': 6379, 'db': 0}
+
+
+class TestCase(unittest.TestCase):
+    def tearDown(self):
+        Redis.connect().flushdb()
