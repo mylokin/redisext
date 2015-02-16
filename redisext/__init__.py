9017048400927f2bbdd5283da2ef98f277a8374b
Andrey Gubarev <mylokin@me.com>
2015-02-16 21:10:37 +0300

    add docs struct

---
+++ redisext/__init__.py
@@ -0,0 +1,28 @@
+'''
+Tutorial
+--------
+
+Models
+------
+
+.. automodule:: redisext.models
+
+.. automodule:: redisext.counter
+
+.. automodule:: redisext.hashmap
+
+.. automodule:: redisext.key
+
+.. automodule:: redisext.pool
+
+.. automodule:: redisext.queue
+
+.. automodule:: redisext.stack
+
+.. automodule:: redisext.serializer
+
+Backend
+-------
+
+.. automodule:: redisext.backend
+'''
--- redisext/counter.py
+++ redisext/counter.py
@@ -1,3 +1,15 @@
+'''
+Counter
+-------
+
+Model allows to build counters in a minute.
+
+Counter API
+-----------
+.. autoclass:: Counter
+   :members:
+
+'''
 from __future__ import absolute_import
 
 import redisext.models.abc
@@ -5,9 +17,19 @@ import redisext.models.abc
 
 class Counter(redisext.models.abc.Model):
     def increment(self):
+        ''' Increment counter by 1.
+
+        :returns:
+        :rtype: int
+        '''
         value = self.connect_to_master().incr(self.key)
         return self.decode(value)
 
     def get(self):
+        ''' Get counter's value.
+
+        :returns: counter's value
+        :rtype: int
+        '''
         value = self.connect_to_slave().get(self.key)
         return self.decode(value)
