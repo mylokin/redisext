0b2c157f05b8f36146417237b1d209f48a547214
Andrey Gubarev <mylokin@me.com>
2015-02-16 20:19:34 +0300

    bump

--- setup.py
+++ setup.py
@@ -6,11 +6,16 @@ except ImportError:
 setup(
     name='redisext',
     packages=['redisext', 'redisext.backend', 'redisext.models'],
-    version='1.1.1',
+    version='1.1.2',
     description='Data models for Redis',
     author='Andrey Gubarev',
     author_email='mylokin@me.com',
     url='https://github.com/mylokin/redisext',
     keywords=['redis', 'orm', 'models'],
-    classifiers=[],
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4'
+    ),
 )
