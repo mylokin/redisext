80c49e03367c49077e2b943dead008ade2f792db
Andrey Gubarev <mylokin@me.com>
2015-02-16 19:27:05 +0300

    update pool #2

--- redisext/tests/test_pool.py
+++ redisext/tests/test_pool.py
@@ -12,22 +12,23 @@ class Pool(fixture.Connection, redisext.pool.Pool):
 
 class PoolTestCase(fixture.TestCase):
     def setUp(self):
+        self.pool = Pool()
         self.data = [1, 2, 3, 4, {'a': 5}]
         self.length = len(self.data)
         for item in self.data:
-            Pool.push(item)
+            self.pool.push(item)
 
     def test_pool_single_pop(self):
-        self.assertIn(Pool.pop(), self.data)
+        self.assertIn(self.pool.pop(), self.data)
 
     def test_pool_multiple_pop(self):
         for x in xrange(self.length):
-            self.assertIn(Pool.pop(), self.data)
+            self.assertIn(self.pool.pop(), self.data)
 
 
 class EmptyPoolTestCase(fixture.TestCase):
     def test_empty_pool(self):
-        self.assertIsNone(Pool.pop())
+        self.assertIsNone(Pool().pop())
 
 
 class KeyPicklePool(fixture.Connection, redisext.pool.Pool):
@@ -46,30 +47,31 @@ class SortedSet(fixture.Connection, redisext.pool.SortedSet):
 class SortedSetTestCase(fixture.TestCase):
     def setUp(self):
         self.data = {'string1': 0, 'string2': 1, 'string3': 2, 'string4': 3}
+        self.sortedset = SortedSet()
         for element, score in self.data.iteritems():
-            SortedSet.add(element, score)
+            self.sortedset.add(element, score)
 
     def test_sortedset_multiple_add(self):
-        self.assertEquals(SortedSet.length(0, 3), 4)
+        self.assertEquals(self.sortedset.length(0, 3), 4)
 
     def test_sortedset_element_availability(self):
         element, score = self.data.iteritems().next()
-        self.assertTrue(SortedSet.contains(element))
+        self.assertTrue(self.sortedset.contains(element))
 
     def test_sortedset_members(self):
         expected_members = sorted(self.data.keys(), reverse=True)
-        self.assertEqual(SortedSet.members(), expected_members)
+        self.assertEqual(self.sortedset.members(), expected_members)
 
     def test_sortedset_truncated_members(self):
-        SortedSet.truncate(2)
+        self.sortedset.truncate(2)
         truncated = sorted(self.data.keys(), reverse=True)[:-2]
-        self.assertEqual(SortedSet.members(), truncated)
+        self.assertEqual(self.sortedset.members(), truncated)
 
     def test_sortedset_clean(self):
-        SortedSet.clean()
-        self.assertEqual(SortedSet.members(), [])
+        self.sortedset.clean()
+        self.assertEqual(self.sortedset.members(), [])
 
 
 class EmptySortedSetTestCase(fixture.TestCase):
     def test_empty_sorted_set(self):
-        self.assertEqual(SortedSet.members(), [])
+        self.assertEqual(SortedSet().members(), [])
