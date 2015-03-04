f9950b58e08825c40d2473f9d2568574b0435992
Andrey Gubarev <mylokin@me.com>
2015-03-05 02:19:27 +0300

    simple reordering

--- redisext/counter.py
+++ redisext/counter.py
@@ -42,15 +42,6 @@ import redisext.models.abc
 
 
 class Counter(redisext.models.abc.Model):
-    def increment(self):
-        ''' Increment counter by 1.
-
-        :returns:
-        :rtype: int
-        '''
-        value = self.connect_to_master().incr(self.key)
-        return self.decode(value)
-
     def get(self):
         ''' Get counter's value.
 
@@ -58,4 +49,13 @@ class Counter(redisext.models.abc.Model):
         :rtype: int
         '''
         value = self.connect_to_slave().get(self.key)
+        return self.decode(value)
+
+    def increment(self):
+        ''' Increment counter by 1.
+
+        :returns:
+        :rtype: int
+        '''
+        value = self.connect_to_master().incr(self.key)
         return self.decode(value)
