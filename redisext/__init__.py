b3502bbd11af14fad3e6f5af1f8f96a334a300e4
Andrey Gubarev <mylokin@me.com>
2015-03-05 02:24:33 +0300

    add tutorial

--- redisext/__init__.py
+++ redisext/__init__.py
@@ -6,6 +6,36 @@ Redisext is a tool for data modeling. Its primary goal is to provide light
 interface to well-known data models based on Redis such as queues, hashmaps,
 counters, pools and stacks. Redisext could be threated like an ORM for Redis.
 
+Tutorial
+--------
+
+Counter Model allows you to build counters in a minute. For example::
+
+   import redisext.backend.redis
+   import redisext.counter
+   import redisext.serializer
+
+   class Connection(redisext.backend.redis.Connection):
+       MASTER = {'host': 'localhost', 'port': 6379, 'db': 0}
+
+   class Visitors(Connection, redisext.Counter):
+       SERIALIZER = redisext.serializer.Numeric
+
+
+This is it! You can start using it. Example of mythical frontpage view::
+
+   def frontpage():
+       visitors_counter = Visitors('fronpage')
+       visitors_counter.increment()
+       context = {
+           'visitors': visitors_counter.get()
+       }
+       return context
+
+.. note::
+
+   Details on :class:`redisext.counter.Counter`.
+
 Data Models
 -----------
 
--- redisext/counter.py
+++ redisext/counter.py
@@ -1,36 +1,6 @@
 '''
 Counter
--------
-
-Model allows to build counters in a minute. For example::
-
-   import redisext.backend.redis
-   import redisext.counter
-   import redisext.serializer
-
-   class Connection(redisext.backend.redis.Connection):
-       MASTER = {'host': 'localhost', 'port': 6379, 'db': 0}
-
-   class Visitors(Connection, redisext.Counter):
-       SERIALIZER = redisext.serializer.Numeric
-
-
-This is it! You can start using it. Example of mythical frontpage view::
-
-   def frontpage():
-       visitors_counter = Visitors('fronpage')
-       visitors_counter.increment()
-       context = {
-           'visitors': visitors_counter.get()
-       }
-       return context
-
-
-.. note::
-   See details about connections and serializers.
-
-Counter API
------------
+^^^^^^^
 
 .. autoclass:: Counter
    :members:
@@ -54,7 +24,7 @@ class Counter(redisext.models.abc.Model):
     def increment(self):
         ''' Increment counter by 1.
 
-        :returns:
+        :returns: counter's value
         :rtype: int
         '''
         value = self.connect_to_master().incr(self.key)
