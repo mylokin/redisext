064802e0354cd9d27a7ea0d1c69a45baf0587c63
Andrey Gubarev <mylokin@me.com>
2015-03-27 00:57:46 +0300

    add pool example

--- redisext/pool.py
+++ redisext/pool.py
@@ -1,15 +1,35 @@
 '''
 Pool
-^^^^
+----
 
 .. autoclass:: Pool
    :members:
 
-SortedSet
-^^^^^^^^^
+The simpliest example of pool usage is token pool::
 
-.. autoclass:: SortedSet
-   :members:
+   class TokenPool(Connection, redisext.pool.Pool):
+      SERIALIZER = redisext.serializer.String
+
+and this pool could be used like::
+
+   >>> facebook = TokenPool('facebook')
+   >>> facebook.push('fb1')
+   True
+   >>> facebook.push('fb1')
+   False
+   >>> facebook.push('fb2')
+   True
+   >>> facebook.pop()
+   u'fb1'
+   >>> facebook.pop()
+   u'fb2'
+   >>> facebook.pop()
+   >>>
+
+SortedSet
+---------
+
+For your spectial needs check :class:`redisext.pool.SortedSet`.
 
 '''
 from __future__ import absolute_import
@@ -19,10 +39,22 @@ import redisext.models.abc
 
 class Pool(redisext.models.abc.Model):
     def pop(self):
+        '''
+        Pop item from pool.
+
+        :returns: obviously item
+        :rtype: how knows(serializer knows)
+        '''
         item = self.connect_to_master().spop(self.key)
         return self.decode(item)
 
     def push(self, item):
+        '''
+        Place item into pool.
+
+        :param item: whatever you need to place into pool
+        :rtype: bool
+        '''
         item = self.encode(item)
         return bool(self.connect_to_master().sadd(self.key, item))
 
